9837d0e6bec991419ba21ee4c6d631a36de96654
Natalia <nataliav_4428@hotmail.com>
2020-11-20 19:59:19 -0600

    "Styles and show services"

--- router/RutasWeb.js
+++ router/RutasWeb.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req,res)=>{
-    res.render('index', {titulo:'mi título dinámico'})
+    res.render('index', {titulo:'Clínica veterinaria'})
 })
 
 router.get('/servicios', (req,res)=>{
-    res.render('servicios', {tituloServicios: "Este es el mensaje dinámico de servicios"})
+    res.render('servicios', {tituloServicios: "Ofrecemos los siguientes servicios:"})
 })
 
 
